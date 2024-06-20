00f7df5fc76ada34967d0fbe37c3cb9ae4973852
KondouM <masaki.private0319@gmail.com>
2024-06-20 10:55:51 +0900

    デプロイ準備

--- jobpit/jobpit/settings.py
+++ jobpit/jobpit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-88@5itrm502!q=e0c$w9-v4yc!7i!lf!_h+3!#tsyv1@@d8i-7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,7 +124,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
